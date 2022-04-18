e7827321bb5177190849262981e12eef64f5e2ca
JoseJEM <thesuperrps3@gmail.com>
2022-04-18 17:34:35 +0200

    cambios a la 17:34
    cambios en la velocidad

--- PlayerControllerX.cs
+++ PlayerControllerX.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class PlayerControllerX : MonoBehaviour
 {
     private Rigidbody playerRb;
-    private float speed = 500;
+    float speed = 350;
     private GameObject focalPoint;
 
     public bool hasPowerup;
